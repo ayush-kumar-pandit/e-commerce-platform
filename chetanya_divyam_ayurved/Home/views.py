90b755c52b1d3d5d99fb0ad4eaec2df0d4f69d74
Ayush <ayushpandit672@gmail.com>
2026-03-09 14:08:55 +0530

    E-commerce

--- chetanya_divyam_ayurved/Home/views.py
+++ chetanya_divyam_ayurved/Home/views.py
@@ -3,6 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 
 def index(request):
-    # dic = {'Client': 'Rahul Bansal',
-    #        'Developer': 'Ayush Pandit'}
-    return render(request, 'index.html')
+    
+    return render(request, 'index.html',context = {'Client': 'Rahul Bansal',
+           'Developer': 'Ayush Pandit'} )
