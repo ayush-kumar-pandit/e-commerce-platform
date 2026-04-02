4787ab98084b0c357596378168b8324d201da675
Ayush <ayushpandit672@gmail.com>
2026-04-02 18:54:09 +0530

    DB Linked

--- chetanya_divyam_ayurved/Home/models.py
+++ chetanya_divyam_ayurved/Home/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class User_Data(models.Model):
+    user_name = models.CharField(max_length=50)
+    password = models.CharField(max_length=50)
+    name = models.CharField(max_length=50)
+    phone = models.IntegerField()
+    mail = models.EmailField(max_length=254)
+    address = models.TextField()
