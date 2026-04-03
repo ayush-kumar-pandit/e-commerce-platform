5d110678e21884190eff775285392bbce02966ce
Ayush <ayushpandit672@gmail.com>
2026-04-04 01:23:07 +0530

    DB added

--- CDA/user/models.py
+++ CDA/user/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class user_data(models.Model):
+    user_name = models.CharField(max_length = 20)
+    name = models.CharField(max_length = 50)
+    password = models.CharField()
+    phone = models.BigIntegerField()
+    email = models.EmailField(max_length=254)
+    address = models.TextField()
