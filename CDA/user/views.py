dcec56db9df2aca9f1a350211107e1ba47b4bc96
Ayush <ayushpandit672@gmail.com>
2026-04-06 05:03:37 +0530

    Optimized

--- CDA/user/views.py
+++ CDA/user/views.py
@@ -4,3 +4,6 @@ from django.http import HttpResponse
 # Create your views here.
 def register(request):
     return render(request, "register.html")
+
+def login(request):
+    return HttpResponse("Login Page")
