c065129a2d7655bb0fa5b5d4192cde17478ca3ae
Ayush <ayushpandit672@gmail.com>
2026-04-16 16:58:22 +0530

    Cart added

--- CDA/user/models.py
+++ CDA/user/models.py
@@ -1,12 +1,37 @@
 from django.db import models
 
-# Create your models here.
 class user_data(models.Model):
-    user_name = models.CharField(max_length = 20)
-    name = models.CharField(max_length = 50)
-    password = models.CharField()
+    user_name = models.CharField(max_length=20)
+    name = models.CharField(max_length=50)
+    password = models.CharField(max_length=100)
     phone = models.BigIntegerField()
     email = models.EmailField(max_length=254)
     address = models.TextField()
     gender = models.TextField()
     age = models.IntegerField()
+
+    def __str__(self):
+        return self.user_name
+
+
+class Cart(models.Model):
+    user = models.OneToOneField(user_data, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.user.user_name}'s Cart"
+
+
+class CartItem(models.Model):
+    cart = models.ForeignKey(Cart, on_delete=models.CASCADE, related_name='items')
+    product_id = models.IntegerField()
+    product_name = models.CharField(max_length=200)
+    product_price = models.DecimalField(max_digits=10, decimal_places=2)
+    product_image = models.ImageField(upload_to='cart_products/', null=True, blank=True)
+    quantity = models.PositiveIntegerField(default=1)
+
+    def __str__(self):
+        return f"{self.product_name} x {self.quantity}"
+
+    @property
+    def total_price(self):
+        return self.product_price * self.quantity
