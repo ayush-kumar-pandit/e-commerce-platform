ec005eb30a30dc4ac445743e8aa9bc6c1928af62
Ayush <ayushpandit672@gmail.com>
2026-04-23 02:26:33 +0530

    Cart issue resolving

--- CDA/user/models.py
+++ CDA/user/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from django.contrib.auth.models import User
+
 
 class user_data(models.Model):
     user_name = models.CharField(max_length=20)
@@ -15,10 +17,10 @@ class user_data(models.Model):
 
 
 class Cart(models.Model):
-    user = models.OneToOneField(user_data, on_delete=models.CASCADE)
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='cart')
 
     def __str__(self):
-        return f"{self.user.user_name}'s Cart"
+        return f"{self.user.username}'s Cart"
 
 
 class CartItem(models.Model):
