090f2ae7e63dacb6d6e870d8df4f150f0c9cd6ad
Ayush <ayushpandit672@gmail.com>
2026-07-08 22:26:55 +0530

    Shop page added

--- CDA/home/urls.py
+++ CDA/home/urls.py
@@ -3,6 +3,7 @@ from .views import *
 
 urlpatterns = [
     path('', home, name='home'),
+    path('shop/', shop_view, name='shop'),
     path('about/', about_view, name='about'),
     path('contact/', contact_view, name='contact'),
     path('our-science/', our_science_view, name='our_science'),
--- CDA/home/views.py
+++ CDA/home/views.py
@@ -1,10 +1,58 @@
 from django.shortcuts import render
+from django.db.models import Q
 from .models import Product, OurScience
 
 # Create your views here.
 def home(request):
     prods = Product.objects.all()
     return render(request,"home.html", {"prods": prods})
+
+def shop_view(request):
+    prods = Product.objects.all()
+
+    # Search
+    q = request.GET.get('q')
+    if q:
+        prods = prods.filter(Q(name__icontains=q) | Q(desc__icontains=q))
+
+    # Filter
+    min_price = request.GET.get('min_price')
+    max_price = request.GET.get('max_price')
+    
+    if min_price:
+        try:
+            prods = prods.filter(price__gte=int(min_price))
+        except ValueError:
+            pass
+            
+    if max_price:
+        try:
+            prods = prods.filter(price__lte=int(max_price))
+        except ValueError:
+            pass
+
+    # Sort
+    sort = request.GET.get('sort')
+    if sort == 'price_asc':
+        prods = prods.order_by('price')
+    elif sort == 'price_desc':
+        prods = prods.order_by('-price')
+    elif sort == 'name_asc':
+        prods = prods.order_by('name')
+    elif sort == 'name_desc':
+        prods = prods.order_by('-name')
+    else:
+        # Default sorting by pk or id to keep order consistent
+        prods = prods.order_by('id')
+
+    context = {
+        'prods': prods,
+        'q': q or '',
+        'min_price': min_price or '',
+        'max_price': max_price or '',
+        'sort': sort or '',
+    }
+    return render(request, "shop.html", context)
 
 def about_view(request):
     return render(request, "about.html")
